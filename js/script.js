8dc0300ddaffc8c358710b623d48852727a1073d
ElenaDutova <eluvarova0997@mail.ru>
2021-02-15 13:47:28 +0300

    lesson 9

--- js/script.js
+++ js/script.js
@@ -8,10 +8,31 @@ console.log(result); */
 /* const answer = +prompt("Your name?", "18");
 console.log(answer + 5); */
 
-const answers = [];
+/* const answers = []; */
 
 /* answers[0] = prompt("What is yor name?", "");
 answers[1] = prompt("What is yor age?", "");
 answers[2] = prompt("What?", ""); */
 
-console.log(typeof(answers));
+/* console.log(typeof(answers)); */
+
+console.log('arr' + " - is obj");
+console.log(4 + +"10");
+
+let incr = 10,
+    decr = 10;
+
+/* ++incr;
+--decr; */
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%3);
+
+console.log(5%3 == 2);
+
+const isCheck = true,
+    isClose = true
+
+    console.log(isCheck && !isClose);
